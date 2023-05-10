087c13410188d4c7bfb508baae8be1a4b3bd9fcc
Leticia-NdO <3972656@alunouninter.com>
2023-05-10 10:24:00 -0300

    refactor: add comments to the adapters in the main layer

--- src/main/adapters/express-middleware-adapter.ts
+++ src/main/adapters/express-middleware-adapter.ts
@@ -1,15 +1,26 @@
 import { HttpRequest, Middleware } from '../../presentation/protocols'
 import { NextFunction, Request, Response } from 'express'
+
+// Function to adapt middleware for Express
 export const adaptMiddleware = (middleware: Middleware) => {
+  // Return an async function that acts as the adapted middleware
   return async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    // Create an instance of HttpRequest using the request headers
     const httpRequest: HttpRequest = {
       headers: req.headers
     }
+
+    // Call the middleware's handle function passing the httpRequest
     const httpResponse = await middleware.handle(httpRequest)
+
+    // Check if the middleware response has a status code of 200 (OK)
     if (httpResponse.statusCode === 200) {
+      // Assign the body of the httpResponse to the Express request object
       Object.assign(req, httpResponse.body)
+      // Call the next function to proceed to the next middleware or route handler
       next()
     } else {
+      // If the status code is not 200, set the corresponding status code on the Express response
       res.status(httpResponse.statusCode).json({
         error: httpResponse.body.message
       })
--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -1,14 +1,24 @@
 import { Controller, HttpRequest } from '../../presentation/protocols'
 import { Request, Response } from 'express'
+
+// Function to adapt a route handler for Express
 export const adaptRoute = (controller: Controller) => {
+  // Return an async function that acts as the adapted route handler
   return async (req: Request, res: Response): Promise<void> => {
+    // Create an instance of HttpRequest using the request body
     const httpRequest: HttpRequest = {
       body: req.body
     }
+
+    // Call the controller's handle function passing the httpRequest
     const httpResponse = await controller.handle(httpRequest)
+
+    // Check if the httpResponse status code is within the range of 200 to 299 (successful response)
     if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+      // Set the status code and send the body of the httpResponse as the response
       res.status(httpResponse.statusCode).json(httpResponse.body)
     } else {
+      // If the status code is outside the range, set the corresponding status code on the Express response
       res.status(httpResponse.statusCode).json({
         error: httpResponse.body.message
       })
