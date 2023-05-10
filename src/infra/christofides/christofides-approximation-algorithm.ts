2ac8f74d3567d5afad49aeb4e20167af71899623
Leticia-NdO <3972656@alunouninter.com>
2023-05-10 10:45:42 -0300

    refactor: add commentos to the christofides algorithm files in the infra layer

--- src/infra/christofides/christofides-approximation-algorithm.ts
+++ src/infra/christofides/christofides-approximation-algorithm.ts
@@ -4,72 +4,77 @@ import { getDistanceBetweenTwoPoints } from '../helpers/get-distance-between-two
 import { ChristofidesVertex } from './christofides-vertex'
 
 export class ChristofidesApproximationAlgorithm implements ApproximationAlgorithm {
-  private spanningTree: CoordinatesWithDistance[]
-  private vertices: Map<number, ChristofidesVertex> = new Map()
-  private time = 0
+  private spanningTree: CoordinatesWithDistance[] // Represents the spanning tree
+  private vertices: Map<number, ChristofidesVertex> = new Map() // Stores vertices of the graph
+  private time = 0 // Used for DFS traversal
 
   getRoute (spanningTree: CoordinatesWithDistance[], startingPointId: number): RoutePointWithSequence[] {
     this.spanningTree = spanningTree
-    this.generateVertices(spanningTree)
-    this.createChristofiderGraph(startingPointId)
+    this.generateVertices(spanningTree) // Generate the vertices from the spanning tree
+    this.createChristofiderGraph(startingPointId) // Create the Christofides graph
 
-    const vertices = this.getVertices()
-    return this.getFinalSequence(vertices)
+    const vertices = this.getVertices() // Get the final vertices after DFS traversal
+    return this.getFinalSequence(vertices) // Generate the final sequence of vertices
   }
 
   private createChristofiderGraph (startingPointId: number): void {
-    const oddVertices = this.getOddVertices()
-    const minimumWeightMatching = this.getMinimumWeightMatching(oddVertices)
-    this.spanningTree = this.spanningTree.concat(minimumWeightMatching)
-    this.generateVertices(this.spanningTree)
-    this.depthFirstSearch(this.vertices.get(startingPointId)!) // aqui colocamos o id do ponto inicial
+    const oddVertices = this.getOddVertices() // Get the odd degree vertices
+    const minimumWeightMatching = this.getMinimumWeightMatching(oddVertices) // Calculate the minimum weight matching
+    this.spanningTree = this.spanningTree.concat(minimumWeightMatching) // Add the matching edges to the spanning tree
+    this.generateVertices(this.spanningTree) // Generate vertices from the updated spanning tree
+    this.depthFirstSearch(this.vertices.get(startingPointId)!) // Perform depth-first search from the starting point
   }
 
   private generateVertices (spanningTree: CoordinatesWithDistance[]): void {
     for (let k = 0; k < spanningTree.length; k++) {
       const edge = spanningTree[k]
-      const sourceDfsVertex = new ChristofidesVertex(edge.source)
-      const destinationDfsVertex = new ChristofidesVertex(edge.destination)
-      this.addVertex(sourceDfsVertex)
-      this.addVertex(destinationDfsVertex)
-      this.addEdge(edge)
-      this.vertices = new Map([...this.vertices.entries()].sort((a, b) => a[1].getNeighbors().length - b[1].getNeighbors().length))
+      const sourceDfsVertex = new ChristofidesVertex(edge.source) // Create a vertex for the source of the edge
+      const destinationDfsVertex = new ChristofidesVertex(edge.destination) // Create a vertex for the destination of the edge
+      this.addVertex(sourceDfsVertex) // Add the source vertex to the graph
+      this.addVertex(destinationDfsVertex) // Add the destination vertex to the graph
+      this.addEdge(edge) // Add the edge between the source and destination vertices
+      this.vertices = new Map(
+        [...this.vertices.entries()].sort(
+          (a, b) => a[1].getNeighbors().length - b[1].getNeighbors().length
+        )
+      ) // Sort the vertices based on the number of neighbors
     }
   }
 
   private addVertex (vertex: ChristofidesVertex): void {
     if (!this.vertices?.has(vertex.getId())) {
-      this.vertices.set(vertex.getId(), vertex)
+      this.vertices.set(vertex.getId(), vertex) // Add the vertex to the graph if it doesn't already exist
     }
   }
 
   private addEdge (edge: CoordinatesWithDistance): void {
     if (this.vertices.has(edge.source.id) && this.vertices.has(edge.destination.id)) {
+      // Check if the vertices of the edge exist in the graph
       for (const [key, value] of this.vertices) {
         if (key === edge.source.id) {
-          value.addNeighbor(edge.destination)
+          value.addNeighbor(edge.destination) // Add the destination as a neighbor of the source
         }
 
         if (key === edge.destination.id) {
-          value.addNeighbor(edge.source)
+          value.addNeighbor(edge.source) // Add the source as a neighbor of the destination
         }
       }
     }
   }
 
   private removeVertex (id: number): void {
-    this.vertices.delete(id)
+    this.vertices.delete(id) // Remove a vertex from the graph by its ID
   }
 
   private getVertices (): Map<number, ChristofidesVertex> {
-    return this.vertices
+    return this.vertices // Get all vertices in the graph
   }
 
   private getOddVertices (): ChristofidesVertex[] {
     const possibleStartingIds: ChristofidesVertex[] = []
     for (const value of this.vertices.values()) {
       if (value.getNeighbors().length % 2 !== 0) {
-        possibleStartingIds.push(value)
+        possibleStartingIds.push(value) // Collect vertices with odd degrees
       }
     }
 
@@ -80,36 +85,53 @@ export class ChristofidesApproximationAlgorithm implements ApproximationAlgorith
     const allOddEdges: CoordinatesWithDistance[] = []
     const minimumWeightMatching: CoordinatesWithDistance[] = []
 
+    // Generate all edges between odd degree vertices
     for (let k = 0; k < oddVertices.length; k++) {
       const origin = oddVertices[k]
       for (let w = 0; w < oddVertices.length; w++) {
         const destiny = oddVertices[w]
-        const distanceBetweenTwoPoints = getDistanceBetweenTwoPoints(origin.getCoordinates(), destiny.getCoordinates())
-        if (distanceBetweenTwoPoints.distance === 0) continue // if the origin and destiny are the same
+        const distanceBetweenTwoPoints = getDistanceBetweenTwoPoints(
+          origin.getCoordinates(),
+          destiny.getCoordinates()
+        )
+        if (distanceBetweenTwoPoints.distance === 0) continue // Skip if the origin and destiny are the same
         allOddEdges.push(distanceBetweenTwoPoints)
       }
     }
 
     for (let k = 0; k < oddVertices.length; k++) {
       const existingMinimumEdge = minimumWeightMatching.filter((edge) => {
-        return edge.source.id === oddVertices[k].getId() || edge.destination.id === oddVertices[k].getId()
+        return (
+          edge.source.id === oddVertices[k].getId() ||
+          edge.destination.id === oddVertices[k].getId()
+        )
       })
 
       if (existingMinimumEdge.length > 0) continue
 
       const allVerticesMinimumEdges = allOddEdges.filter((edge) => {
-        return edge.destination === oddVertices[k].getCoordinates() || edge.source === oddVertices[k].getCoordinates()
+        return (
+          edge.destination === oddVertices[k].getCoordinates() ||
+          edge.source === oddVertices[k].getCoordinates()
+        )
       })
 
       allVerticesMinimumEdges.sort((a, b) => a.distance - b.distance)
 
       for (let k = 0; k < allVerticesMinimumEdges.length; k++) {
         const shortestPath = allVerticesMinimumEdges[k]
-        if (minimumWeightMatching.find((item) => {
-          return item.destination.id === shortestPath.destination.id || item.source.id === shortestPath.source.id || item.source.id === shortestPath.destination.id || item.destination.id === shortestPath.source.id
-        })) continue
+        if (
+          minimumWeightMatching.find((item) => {
+            return (
+              item.destination.id === shortestPath.destination.id ||
+              item.source.id === shortestPath.source.id ||
+              item.source.id === shortestPath.destination.id ||
+              item.destination.id === shortestPath.source.id
+            )
+          })
+        ) { continue } // Skip if the edge is already present in the minimum weight matching
 
-        minimumWeightMatching.push(shortestPath)
+        minimumWeightMatching.push(shortestPath) // Add the shortest path to the minimum weight matching
       }
     }
 
@@ -118,22 +140,22 @@ export class ChristofidesApproximationAlgorithm implements ApproximationAlgorith
 
   private depthFirstSearch (initialVertex: ChristofidesVertex): void {
     this.time = 1
-    this.dfs(initialVertex)
+    this.dfs(initialVertex) // Perform depth-first search traversal
   }
 
   private dfs (vertex: ChristofidesVertex): void {
-    vertex.setColor('red')
+    vertex.setColor('red') // Mark the vertex as visited (red color)
     vertex.setDiscoveryTime(this.time)
     this.time += 1
     const vertexNeighbors = vertex.getNeighbors()
     for (let k = 0; k < vertexNeighbors.length; k++) {
       const neighbor = this.vertices.get(vertexNeighbors[k].id)
       if (neighbor?.getColor() === 'black') {
-        this.dfs(neighbor)
+        this.dfs(neighbor) // Recursively visit unvisited neighbors
       }
     }
 
-    vertex.setColor('blue')
+    vertex.setColor('blue') // Mark the vertex as finished (
     vertex.setFinishTime(this.time)
     this.time += 1
   }
@@ -142,6 +164,7 @@ export class ChristofidesApproximationAlgorithm implements ApproximationAlgorith
     const finalRoute: RoutePointWithSequence[] = []
     let sequence = 0
     const sortedMap = new Map([...verticesMap.entries()].sort((a, b) => a[1].getDiscoveryTime() - b[1].getDiscoveryTime()))
+
     for (const value of sortedMap.values()) {
       finalRoute.push({
         coordinates: value.getCoordinates(),
@@ -149,11 +172,11 @@ export class ChristofidesApproximationAlgorithm implements ApproximationAlgorith
       })
     }
 
-    this.cleanVertices()
+    this.cleanVertices() // Clean up the graph vertices
     return finalRoute
   }
 
   private cleanVertices (): void {
-    this.vertices = new Map()
+    this.vertices = new Map() // Reset the vertices map
   }
 }
--- src/infra/christofides/christofides-vertex.ts
+++ src/infra/christofides/christofides-vertex.ts
@@ -1,54 +1,52 @@
 import { Coordinate } from '../../domain/models'
 
 export class ChristofidesVertex {
-  private readonly neighbors: Coordinate[] = []
-  private discovery: number = 0
-  private finish: number = 0
-  private color: string = 'black'
+  private readonly neighbors: Coordinate[] = [] // Array to store the neighboring vertices
+  private discovery: number = 0 // Discovery time during depth-first search
+  private finish: number = 0 // Finish time during depth-first search
+  private color: string = 'black' // Color used during depth-first search
 
-  constructor (
-    private readonly coordinates: Coordinate
-  ) {}
+  constructor (private readonly coordinates: Coordinate) {}
 
   addNeighbor (coordinates: Coordinate): void {
     if (!this.neighbors.includes(coordinates)) {
-      this.neighbors.push(coordinates)
+      this.neighbors.push(coordinates) // Add a neighboring vertex to the current vertex
     }
   }
 
   getId (): number {
-    return this.coordinates.id
+    return this.coordinates.id // Get the ID of the vertex
   }
 
   setColor (color: string): void {
-    this.color = color
+    this.color = color // Set the color of the vertex during depth-first search
   }
 
   getColor (): string {
-    return this.color
+    return this.color // Get the color of the vertex during depth-first search
   }
 
   setDiscoveryTime (discoveryTime: number): void {
-    this.discovery = discoveryTime
+    this.discovery = discoveryTime // Set the discovery time of the vertex during depth-first search
   }
 
   getDiscoveryTime (): number {
-    return this.discovery
+    return this.discovery // Get the discovery time of the vertex during depth-first search
   }
 
   setFinishTime (finishTime: number): void {
-    this.finish = finishTime
+    this.finish = finishTime // Set the finish time of the vertex during depth-first search
   }
 
   getFinishTime (): number {
-    return this.finish
+    return this.finish // Get the finish time of the vertex during depth-first search
   }
 
   getCoordinates (): Coordinate {
-    return this.coordinates
+    return this.coordinates // Get the coordinates of the vertex
   }
 
   getNeighbors (): Coordinate[] {
-    return this.neighbors
+    return this.neighbors // Get the neighboring vertices of the current vertex
   }
 }
