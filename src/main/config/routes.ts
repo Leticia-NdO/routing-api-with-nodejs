b863e66d5962215e4ad90c250f563378421e5727
Leticia-NdO <3972656@alunouninter.com>
2023-05-10 10:26:14 -0300

    refactor: add comments to the routes configuration file

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -2,15 +2,18 @@ import { Express, Router } from 'express'
 import { readdirSync } from 'fs'
 import path from 'path'
 
+// Function to configure routes for the app
 export default (app: Express): void => {
   const router = Router()
+
+  // Mount the router under the '/api/v1' base path
   app.use('/api/v1', router)
-  // fg.sync('**/src/main/routes/**routes.ts').map(async file => {
-  //   const route = (await import(`../../../${file}`)).default // o import default desss arquivos é uma função que espera o router como parâmetro
-  //   route(router)
-  // })
-  readdirSync(path.join(`${__dirname}`, '/../routes')).map(async file => {
+
+  // Read the files in the routes directory and map over them
+  readdirSync(path.join(`${__dirname}`, '/../routes')).map(async (file) => {
+    // Check if the file name does not include '.test.'
     if (!file.includes('.test.')) {
+      // Dynamically import the route file and call its default export with the router
       (await import(`../routes/${file}`)).default(router)
     }
   })
